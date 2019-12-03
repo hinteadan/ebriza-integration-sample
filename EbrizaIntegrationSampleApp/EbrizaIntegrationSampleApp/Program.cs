dd2b27b6a781e4eaf919aea1061c3c2ed778e8c1
Hintee <dan.hintea@ebriza.com>
2019-12-03 12:29:11 +0200

    Scaffold C# Solution

--- EbrizaIntegrationSampleApp/EbrizaIntegrationSampleApp/Program.cs
+++ EbrizaIntegrationSampleApp/EbrizaIntegrationSampleApp/Program.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace EbrizaIntegrationSampleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => MainAsync(args).Wait();
+        static async Task MainAsync(string[] args)
         {
+            await Task.Delay(1000);
+
+            Console.WriteLine($"Done @ {DateTime.Now}");
+            Console.ReadLine();
         }
     }
 }
