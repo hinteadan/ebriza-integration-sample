9d99adedb15ff15c579961c563f4e2ee092326c9
Hintee <dan.hintea@ebriza.com>
2019-12-03 12:54:39 +0200

    OD: Implement integration sample

--- EbrizaIntegrationSampleApp/EbrizaIntegrationSampleApp/Program.cs
+++ EbrizaIntegrationSampleApp/EbrizaIntegrationSampleApp/Program.cs
@@ -5,6 +5,21 @@ namespace EbrizaIntegrationSampleApp
 {
     class Program
     {
+        #region App Integration Keys
+        /*
+         * 
+         * All these keys are available from the Company Account on which the app is installed
+         * 
+         * appPublicKey - identifies the app itself; is unique for each Ebriza App
+         * appSecretKey - identifies the app itself (serves as the app's password); is unique for each Ebriza App
+         * clientId - identifies the Ebriza Client Company Account on which the app is installed (e.g.: Meron, Narcofee, etc.); is unique for an Ebriza Client;
+         * 
+         * */
+        const string appPublicKey = "53c1c668-f3b3-486f-9652-6a327819da0d";
+        const string appSecretKey = "53c1c668-f3b3-486f-9652-6a327819da0d";
+        const string clientId = "53c1c668-f3b3-486f-9652-6a327819da0d";
+        #endregion
+
         static void Main(string[] args) => MainAsync(args).Wait();
         static async Task MainAsync(string[] args)
         {
